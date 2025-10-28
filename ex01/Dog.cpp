d1022ed1ef84def7cac4015dc3ea9d419cb55e62
Batrii <dhj.nafiai@gmail.com>
2025-10-28 15:47:13 +0100

    needs some theory undertanding, but its finished

---
+++ ex01/Dog.cpp
@@ -0,0 +1,34 @@
+#include "Dog.hpp"
+
+Dog::Dog()
+{
+    this->brain = new Brain();
+    this->type = "Dog";
+    std::cout << "Dog default constructor called" << std::endl;
+}
+Dog::Dog(const Dog& other)
+{
+    this->brain = new Brain();
+    this->brain->operator=(*other.brain);
+    this->type = other.type;
+    std::cout << "Dog copy constructor called" << std::endl;
+}
+Dog& Dog::operator=(const Dog& other)
+{
+    if (this != &other)
+    {
+        this->brain->operator=(*other.brain);
+        this->type = other.type;
+    }
+    std::cout << "Dog assignement operator called" << std::endl;
+    return *this;
+}
+Dog::~Dog()
+{
+    delete this->brain;
+    std::cout << "Dog destructor called" << std::endl;
+};
+void Dog::makeSound() const
+{
+    std::cout << "Woof Woof" << std::endl;
+};
